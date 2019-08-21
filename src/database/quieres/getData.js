e3e057a5ab47d19c91563ba3fb701d40cf143de1
rana <ranasobeid95@gmail.com>
2019-08-21 11:31:44 +0300

    refactor handlebars files create form and handle route display location info which get from database in our home page Relates #11

--- src/database/quieres/getData.js
+++ src/database/quieres/getData.js
@@ -1,6 +1,6 @@
 const dbconnection = require('../config/connection');
 
 exports.getData = (req, res) => dbconnection
-  .query('select * from users')
-  .then((result) => console.log(result.rows))
+  .query('select * from location')
+  .then((result) => res.render('home', { data: result.rows }))
   .catch((err) => console.log(err.stack));
