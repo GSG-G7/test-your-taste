a802c987b8b6c260899643a0efa76e97fe103ecb
Asmaathabet <asmathabet2009@hotmail.com>
2019-08-21 11:14:10 +0300

    make test for getData function from db relates #14

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -1,8 +1,18 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
+let dbUrl = '';
+
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = process.env.test_taste_DB;
+} else {
+  dbUrl = process.env.taste_DB;
+}
+
+if (!dbUrl) throw new Error('No Database URL!!!');
+
 const options = {
-  connectionString: process.env.taste_DB,
+  connectionString: dbUrl,
   ssl: true,
 };
 module.exports = new Pool(options);
--- test/test.js
+++ test/test.js
@@ -1,6 +1,28 @@
 const test = require('tape');
+const { dbBuild } = require('../src/database/config/build');
+const { getData } = require('../src/database/quieres/getData');
 
-test('inital test', t => {
-    t.equal(2, 2, "must be equal");
-    t.end();
-})
+test('inital test', (t) => {
+  t.equal(2, 2, 'must be equal');
+  t.end();
+});
+
+test('test getData query', (t) => {
+  // console.log(getData());
+  dbBuild()
+    .then(() => getData())
+    .then((result) => {
+      const expected = [
+        { id: 1, name: 'Hashem', email: 'h123@gmail.com' },
+        { id: 2, name: 'asmaa', email: 'asa@hotmail.com' },
+      ];
+      t.deepEqual(result, expected, 'should be the same result');
+      t.end();
+    })
+    .catch((err) => {
+      t.error(err);
+      t.end();
+    });
+});
+
+test.onFinish(() => process.exit(0));
