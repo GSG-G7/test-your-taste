9c2029e1fb7f7b119108238fae74533ba0e8b065
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-08-22 10:30:28 +0300

    invoke the function twice issue resolved Relates #31

--- src/database/quieres/getData.js
+++ src/database/quieres/getData.js
@@ -1,6 +1,3 @@
 const dbconnection = require('../config/connection');
 
-exports.getData = () => dbconnection
-  .query('select * from location')
-  .then((result) => (result.rows))
-  .catch((err) => console.log(err.stack));
+exports.getData = () => dbconnection.query('select * from location');
